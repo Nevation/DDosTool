ef099caa10108a4b5cee13c0fbfd323c1d04775a
ltshub <skxotn1@naver.com>
2019-08-27 15:40:48 +0900

    wrong spell

--- PacketManager.h
+++ PacketManager.h
@@ -17,7 +17,7 @@ public:
     vector<vector<uchar>> GetPackets();
     int GetPacketCnt();
     uint16_t tcpCheckSum(uint16_t len_tcp, uint16_t src_addr[], uint16_t dest_addr[], BOOL padding, uint16_t buff[]);
-    uint16_t tcpCheckSum(uint16_t len_tcp, uint16_t src_addr[], uint16_t dest_addr[], BOOL padding, uint16_t buff[]);
+    uint16_t UdpCheckSum(uint16_t len_tcp, uint16_t src_addr[], uint16_t dest_addr[], BOOL padding, uint16_t buff[]);
 
 
 private:
